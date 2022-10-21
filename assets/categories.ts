4055bdc911d8e15061440a14d45ffdda5d83d020
batuctn <batuctnn14@gmail.com>
2022-10-21 15:28:02 +0300

    index id fixed

--- assets/categories.ts
+++ assets/categories.ts
@@ -1,6 +1,6 @@
 export default [
   {
-    id: "1",
+    id: "0",
     name: "İndirimler",
     src: "https://cdn.getir.com/cat/5fd8c58f3bdc2389a56e0fb0_2b1a70ca-4a4a-4477-adf1-7414a476aff8.jpeg",
     subCategories: [
@@ -13,7 +13,7 @@ export default [
     ],
   },
   {
-    id: "2",
+    id: "1",
     name: "Su & İçecek",
     src: "https://cdn.getir.com/cat/551430043427d5010a3a5c5e_1619242669958_1619242670038.jpeg",
     subCategories: [
@@ -26,7 +26,7 @@ export default [
     ],
   },
   {
-    id: "3",
+    id: "2",
     name: "Meyve & Sebze",
     src: "https://cdn.getir.com/cat/5928113e616cab00041ec6de_1619242870968_1619242871055.jpeg",
     subCategories: [
@@ -39,7 +39,7 @@ export default [
     ],
   },
   {
-    id: "4",
+    id: "3",
     name: "Fırından",
     src: "https://cdn.getir.com/cat/566eeb85f9facb0f00b1cb16_1619242817768_1619242817849.jpeg",
     subCategories: [
@@ -52,7 +52,7 @@ export default [
     ],
   },
   {
-    id: "5",
+    id: "4",
     name: "Temel Gıda",
     src: "https://cdn.getir.com/cat/56dfcfba86004203000a870d_1619242834654_1619242834734.jpeg",
     subCategories: [
@@ -65,7 +65,7 @@ export default [
     ],
   },
   {
-    id: "6",
+    id: "5",
     name: "Atıştırmalık",
     src: "https://cdn.getir.com/cat/56dfe03cf82055030022cdc0_1619242841966_1619242842053.jpeg",
     subCategories: [
@@ -78,7 +78,7 @@ export default [
     ],
   },
   {
-    id: "7",
+    id: "6",
     name: "Dondurma",
     src: "https://cdn.getir.com/cat/55bca8484dcda90c00e3aa80_1619242741382_1619242741482.jpeg",
     subCategories: [
@@ -91,7 +91,7 @@ export default [
     ],
   },
   {
-    id: "8",
+    id: "7",
     name: "Süt Ürünleri",
     src: "https://cdn.getir.com/cat/5cfe685e25d48100016b7ea6_b05535e0-a936-4c79-8d89-10db178a928c.jpeg",
     subCategories: [
@@ -104,7 +104,7 @@ export default [
     ],
   },
   {
-    id: "9",
+    id: "8",
     name: "Kahvaltılık",
     src: "https://cdn.getir.com/cat/61a78ebbdea800061135fd19_1ae7279d-6ab8-4ee1-b58b-9a29695ad314.jpeg",
     subCategories: [
@@ -117,7 +117,7 @@ export default [
     ],
   },
   {
-    id: "10",
+    id: "9",
     name: "Yiyecek",
     src: "https://cdn.getir.com/cat/551430043427d5010a3a5c5d_1619242660025_1619242660107.jpeg",
     subCategories: [
@@ -130,7 +130,7 @@ export default [
     ],
   },
   {
-    id: "11",
+    id: "10",
     name: "Fit & Form",
     src: "https://cdn.getir.com/cat/57e2996551f3ee030027e28b_1619242858559_1619242858642.jpeg",
     subCategories: [
@@ -143,7 +143,7 @@ export default [
     ],
   },
   {
-    id: "12",
+    id: "11",
     name: "Kişisel Bakım",
     src: "https://cdn.getir.com/cat/551430043427d5010a3a5c5c_1619242651249_1619242651335.jpeg",
     subCategories: [
@@ -156,7 +156,7 @@ export default [
     ],
   },
   {
-    id: "13",
+    id: "12",
     name: "Evcil Hayvan",
     src: "https://cdn.getir.com/cat/551430043427d5010a3a5c63_1619242711604_1619242711687.jpeg",
     subCategories: [
@@ -169,7 +169,7 @@ export default [
     ],
   },
   {
-    id: "14",
+    id: "13",
     name: "Bebek",
     src: "https://cdn.getir.com/cat/551430043427d5010a3a5c5b_1619242620186_1619242620271.jpeg",
     subCategories: [
@@ -182,7 +182,7 @@ export default [
     ],
   },
   {
-    id: "15",
+    id: "14",
     name: "Cinsel Sağlık",
     src: "https://cdn.getir.com/cat/559c07b093be370c0063dd29_a16cbca7-4f5a-46fc-8f45-45d8e899f57d.jpeg",
     subCategories: [
@@ -195,7 +195,7 @@ export default [
     ],
   },
   {
-    id: "16",
+    id: "15",
     name: "Ev Bakım",
     src: "https://cdn.getir.com/cat/55449fdf02632e11003c2da8_1619242719523_1619242719602.jpeg",
     subCategories: [
@@ -208,7 +208,7 @@ export default [
     ],
   },
   {
-    id: "17",
+    id: "16",
     name: "Teknoloji",
     src: "https://cdn.getir.com/cat/551430043427d5010a3a5c62_1619242697597_1619242697702.jpeg",
     subCategories: [
@@ -221,7 +221,7 @@ export default [
     ],
   },
   {
-    id: "18",
+    id: "17",
     name: "Ev & Yaşam",
     src: "https://cdn.getir.com/cat/5b06b056b883b700044e3e4c_1619242916956_1619242917048.jpeg",
     subCategories: [
