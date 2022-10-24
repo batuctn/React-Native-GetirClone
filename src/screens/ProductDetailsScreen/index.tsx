bb3af80824e05d4d75f712e5d7cc4fc3b59401e5
batuctn <batuctnn14@gmail.com>
2022-10-24 16:25:28 +0300

    product details screen add with component and props

--- src/screens/ProductDetailsScreen/index.tsx
+++ src/screens/ProductDetailsScreen/index.tsx
@@ -1,12 +1,46 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { View, Text, ScrollView, ActivityIndicator } from "react-native";
+import { Product } from "../../models";
+import ImageCarousel from "../../components/ImageCarousel";
+import DetailBox from "../../components/DetailBox";
+import DetailProperty from "../../components/DetailProperty";
+import CardButton from "../../components/CardButton";
 
-const Index = () => {
+function Index(props) {
+  const [product, setProduct] = useState<Product>();
+  useEffect(() => {
+    setProduct(props.route.params.product);
+  }, []);
+
+  if (!product) {
+    return <ActivityIndicator color={"#5D3EBD"} />;
+  }
   return (
-    <View>
-      <Text>Index</Text>
+    <View style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1, backgroundColor: "#f5f5f5" }}>
+        <ImageCarousel images={product.images} />
+        <DetailBox
+          price={product.fiyat}
+          name={product.name}
+          quantity={product.miktar}
+        />
+        <Text
+          style={{
+            paddingHorizontal: 10,
+            paddingVertical: 13,
+            color: "#808B99",
+            fontWeight: "600",
+            fontSize: 14,
+            marginLeft: 6,
+          }}
+        >
+          Detaylar
+        </Text>
+        <DetailProperty />
+      </ScrollView>
+      <CardButton product={product} />
     </View>
   );
-};
+}
 
 export default Index;
